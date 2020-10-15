f98d7b635a757d8cb3a4e82a529396120004e27a
LandryECAM <17046@student.ecam.be>
2020-10-15 14:13:22 +0200

    Complete exo 2

--- exercises/exo2/reader.go
+++ exercises/exo2/reader.go
@@ -16,6 +16,18 @@ func main() {
 	io.Copy(os.Stdout, &r)
 }
 
+func (x spaceEraser) Read(L []byte) (int, error) {
+	length, err := x.r.Read(L)
+	j := 0 //compteur
+	for i := 0; i < length; i++ {
+		if L[i] != 32 { // test si le charactère n'est pas un espace
+			L[j] = L[i] // on l'ajoute dans la liste L
+			j++
+		}
+	}
+	return j, err
+}
+
 // Implement a type that satisfies the io.Reader interface and reads from another io.Reader,
 // modifying the stream by removing the spaces.
 // Expected output: "Helloworld!"
